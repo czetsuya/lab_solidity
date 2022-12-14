3c36126a512ad758b0a5ac5afccf92cf7df2f2d5
Edward P. Legaspi <czetsuya@gmail.com>
2022-12-14 18:50:02 +0800

    bump: versions

--- deploy.js
+++ deploy.js
@@ -1,10 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const {interface, bytecode} = require('./compile');
 
-const provider = new HDWalletProvider(
-  'xxx',
-  'https://goerli.infura.io/v3/xxx'
+const {abi, evm} = require('./compile');
+
+provider = new HDWalletProvider(
+'',
+  ''
 );
 
 const web3 = new Web3(provider);
@@ -14,8 +15,8 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-  .deploy({data: bytecode, arguments: ['Hi there!']})
+  const result = await new web3.eth.Contract(abi)
+  .deploy({data: evm.bytecode.object, arguments: ['Hi there!']})
   .send({gas: '1000000', from: accounts[0]});
 
   console.log('Contract deployed to', result.options.address);
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,18 +2,20 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const {interface, bytecode} = require('../compile');
+
+const {abi, evm} = require('../compile');
 
 let accounts;
 let inbox;
 
 beforeEach(async () => {
-  // get a list of all accounts
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-  .deploy({data: bytecode, arguments: ['Hi Ed!']})
+  inbox = await new web3.eth.Contract(abi)
+  .deploy({
+    data: evm.bytecode.object,
+    arguments: ['Hi there!'],
+  })
   .send({from: accounts[0], gas: '1000000'});
 });
 
@@ -21,16 +23,13 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
-
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal('Hi Ed!', message);
+    assert.equal(message, 'Hi there!');
   });
-
   it('can change the message', async () => {
-    await inbox.methods.setMessage('Hi Kerri!').send({from: accounts[0]});
-
+    await inbox.methods.setMessage('bye').send({from: accounts[0]});
     const message = await inbox.methods.message().call();
-    assert.equal('Hi Kerri!', message);
+    assert.equal(message, 'bye');
   });
 });
